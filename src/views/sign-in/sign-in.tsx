a66dd8b3cc52af709cd9cffc04ca7a21444a17f6
Karol66 <karolbal66@gmail.com>
2024-03-22 18:11:19 +0100

    ITMS-25 Ui done

--- src/views/sign-in/sign-in.tsx
+++ src/views/sign-in/sign-in.tsx
@@ -2,11 +2,31 @@ import { useNavigate } from "react-router-dom";
 import "./sign-in.scss";
 
 export const SignIn = () => {
-	const navigate = useNavigate();
-
+	const navigate = useNavigate()
 	return (
-		<div className='sign-in'>
-			SignIn <button onClick={() => navigate("/")}>test to home</button>
+		<div className="loginWrapper">
+			<div className="login">
+				<h1>Sign in</h1>
+				<div className="login-input">
+					<input type="text" placeholder="Username" required />
+				</div>
+				<div className="login-input">
+					<input type="text" placeholder="Password" required />
+				</div>
+
+				<div className="remember-checkbox">
+					<label>
+						<input type="checkbox" /> Remember me
+					</label>
+					<a href="#">Forgot password?</a>
+				</div>
+
+				<button>Login</button>
+
+				<div className="register-link">
+					<p>Don't have an account? <button onClick={()=>navigate("/register")}>Register</button></p>
+				</div>
+			</div>
 		</div>
 	);
 };
