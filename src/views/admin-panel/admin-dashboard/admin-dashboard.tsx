8df50e5c19953af55caafe6f64a1b2aec3ac78a0
damdam6680 <damdam6680@gmail.com>
2024-04-07 17:24:11 +0200

    ITMS-99 dodanie dashboard

---
+++ src/views/admin-panel/admin-dashboard/admin-dashboard.tsx
@@ -0,0 +1,76 @@
+import { Col, Row } from "antd";
+import * as React from "react";
+import {
+    Dashboarduser,
+  DashbordTotalCountCard,
+  Products,
+  Tasks,
+} from "../../../components/home";
+
+export const Admindashboard = () => {
+
+    const [isLoading, setIsLoading] = React.useState(false)
+
+  return (
+    <div>
+      <Row gutter={[32, 32]} style={{
+        paddingTop: '20px'
+      }}>
+        <Col xs={24} sm={24} xl={8} >
+          <DashbordTotalCountCard resource="task" isLoading={isLoading} totalCount={25} />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashbordTotalCountCard resource="product" isLoading={isLoading} totalCount={225} />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashbordTotalCountCard resource="user" isLoading={isLoading} totalCount={125} />
+        </Col>
+      </Row>
+
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: "32px",
+        }}
+      ></Row>
+
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: "32px",
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
+        >
+          <Tasks />
+        </Col>
+
+        <Col
+          xs={24}
+          sm={24}
+          xl={16}
+          style={{
+            height: "460px",
+          }}
+        >
+          <Products />
+        </Col>
+      </Row>
+
+     <Row gutter={[32, 32]} style={{ marginTop: '32px'}}>
+        <Col xs={24}>
+            <Dashboarduser />
+        </Col>
+     </Row>
+
+    </div>
+  );
+};
+
+export default Admindashboard;
